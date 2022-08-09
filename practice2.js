c8b3397084c4e72a8a0e475bf24d76044abaa447
mharif1 <md5667676@gmail.com>
2022-08-09 23:17:47 +0600

    problem 2 sloved

---
+++ practice2.js
@@ -0,0 +1,9 @@
+const Mathematics =75.25;
+const Biology = 65 ;
+const Chemistry = 80 ;
+const Physics =35.45;
+const bangla =99.50 ;
+
+const totalnumber = Mathematics + Biology + Chemistry + Physics + bangla ;
+const avarageNumber = totalnumber / 5 ;
+console.log(avarageNumber.toFixed(2));
