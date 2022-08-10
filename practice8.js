259cd657f39c9b505a60e268e5232b7af29aa23d
mharif1 <md5667676@gmail.com>
2022-08-11 00:23:36 +0600

    as4p1 sloved

---
+++ practice8.js
@@ -0,0 +1,10 @@
+function radianToDegree(radian) {
+    return radian * (180 / Math.PI);
+  }
+let radian1=radianToDegree(10)
+let radian2=radianToDegree(25)
+let radian3=radianToDegree(199)
+
+console.log(radian1.toFixed(2))
+console.log(radian2.toFixed(2))
+console.log(radian3.toFixed(2))
